bfb42f39ed6f5182f97d46cde1adaa64ec8218d9
mwxood <mwxood@gmail.com>
2024-04-13 08:39:40 +0300

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
-      frontend: './js/index.js',
+    frontend: './js/index.js',
   },
 
   output: {
@@ -14,82 +14,77 @@ module.exports = {
 
   module: {
     rules: [
-        {
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
-          {
-
-            test: /\.(sa|sc|c)ss$/,
-      
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader
-                  }, 
-                   {
-                     loader: "css-loader",
-                   },
-                   {
-
-                     loader: "postcss-loader"
-                   },
-                   {
-
-                     loader: "sass-loader",
-                     options: {
-                       implementation: require("sass")
-                     }
-                   }
-                 ]
-          },
-          {
-            // Apply rule for fonts files
-            test: /\.(woff|woff2|ttf|otf|eot)$/,
-            use: [
-                   {
-                     // Using file-loader too
-                     loader: "file-loader",
-                     options: {
-                       publicPath: '../fonts',
-                       outputPath: 'fonts'
-                     }
-                   }
-                 ]
-          },
-
-          {
-            // Now we apply rule for img
-            test: /\.(png|jpe?g|gif|svg)$/,
-            use: [
-                   {
-                     loader: "file-loader",
-                     options: {
-                        outputPath: 'img',
-                       //   path: path.resolve(__dirname, './img'),
-                         publicPath: '../img',
-                         // path: path.resolve(__dirname, 'img'),
-                     }
-                   }
-                 ]
-          },
-         
-    ],
-},
-
-plugins: [
-    new MiniCssExtractPlugin({
-        options: {
-            outputPath: 'css'
         },
-      filename: "css/[name].bundle.css"
-    })
+      },
+      {
+        test: /\.scss$/,
+
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        // Apply rule for fonts files
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            // Using file-loader too
+            loader: 'file-loader',
+            options: {
+              publicPath: '../fonts',
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
+
+      {
+        // Now we apply rule for img
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img',
+              //   path: path.resolve(__dirname, './img'),
+              publicPath: '../img',
+              // path: path.resolve(__dirname, 'img'),
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'css/[name].bundle.css',
+    }),
   ],
 
-  mode: 'development'
+  mode: 'development',
 };
